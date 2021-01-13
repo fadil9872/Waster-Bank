ed0e2a2ab2d9a36e101842e4270e3d3a4ee25cb7
romi9872 <adian281r@gmail.com>
2021-01-13 13:18:12 +0700

    fix bug & tambah alamat

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(RoleSeeder::class);
-        // $this->call(UserSeeder::class);
-        // $this->call(WilayahSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(WilayahSeeder::class);
         $this->call(AlamatSeeder::class);
         $this->call(SaldoSeeder::class);
         $this->call(SampahSeeder::class);

e0af8e17baab1685dda9aa343decc4e1b54a960b
romi9872 <adian281r@gmail.com>
2021-01-14 20:06:33 +0700

    fix bug

--- database/seeds/PendataanSeeder.php
+++ database/seeds/PendataanSeeder.php
@@ -12,8 +12,8 @@ class PendataanSeeder extends Seeder
     public function run()
     {
         DB::insert("INSERT INTO permintaans (id, user_id, pengurus_id, alamat_id, nama, lokasi, wilayah_id, no_telpon, keterangan, status, tanggal, created_at, updated_at) VALUES 
-        (NULL, '7', NULL, '7', 'nasabah', 'Tirtohargo, Kretek', '3402030', '08522356616', 'datang', '1', '2021-01-14', '2021-01-14 19:14:52', '2021-01-14 19:14:52'),
-        (NULL, '7', NULL, '7', 'nasabah', 'Tirtohargo, Kretek', '3402030', '08522356616', 'datang', '1', '2021-01-14', '2021-01-14 19:14:52', '2021-01-14 19:14:52');
+        (1, '7', NULL, '7', 'nasabah', 'Tirtohargo, Kretek', '3402030', '08522356616', 'datang', '1', '2021-01-14', '2021-01-14 19:14:52', '2021-01-14 19:14:52'),
+        (2, '7', NULL, '7', 'nasabah', 'Tirtohargo, Kretek', '3402030', '08522356616', 'datang', '1', '2021-01-14', '2021-01-14 19:14:52', '2021-01-14 19:14:52');
          ");
     }
 
